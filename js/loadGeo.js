6b18bf38650bcbb0792412a74a9cbad0639882a3
timuric <timuric@gmail.com>
2014-04-27 12:32:58 +0200

    bug fixes

--- js/loadGeo.js
+++
@@ -1,8 +0,0 @@
-var loadGeo = function(data, key){
-	for (var i = 0, l = [selection length]; i < l; i++) {
-		var layer = selection[i];
-		var randomName = data[ Math.floor(Math.random() * data.length) ][key];
-		[layer setStringValue: randomName];
-		[layer setName: randomName];
-	}
-};
