0e6dcf7cc204dbcdddd369f9b7411c5ef1df08bd
Joshua Mauldin <hello@joshuamauldin.com>
2015-05-28 18:44:51 -0400

    adding blank text and image template

---
+++ data/text/my-text.js
@@ -0,0 +1,10 @@
+// FORMATTING NOTE: //
+// 
+// Make sure to separate multiple lines with a comma
+// The last line should not have a comma
+// 
+// Have fun! //
+
+data = [
+"Reveal the plugins folder, open Content Generator Sketch Plugin, then paste your text into the this file: Data > Text > my-text.js"
+];
