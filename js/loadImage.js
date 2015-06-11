95306198a68db16d875ecdd8036e2f915fa667be
Timur <timur@appseven.nl>
2015-06-11 02:42:09 +0200

    Generate unique when possible pictures

--- js/loadImage.js
+++ js/loadImage.js
@@ -2,81 +2,72 @@ var loadImages = function(dataPath, groupName, pictureName){
 	var groupName = groupName || 'picture';
 		pictureName = pictureName || 'picture';
 
-	function loadImages(selectedImgCount){
-		log('selectedImgCount: '+selectedImgCount);
+	function loadImages(imgAmount){
+		var fileManager = [NSFileManager defaultManager];
+		var scriptPath = sketch.scriptPath;			
+		var pluginFolder = scriptPath.match(/Plugins\/([\w -])*/)[0] + "/";
+		var sketchPluginsPath = scriptPath.replace(/Plugins([\w \/ -])*.sketchplugin$/, "");
+		var imagesPath =  sketchPluginsPath + pluginFolder + dataPath;
+		var imagesFileNames = [fileManager contentsOfDirectoryAtPath:imagesPath error:nil];
+		var imageCount = [imagesFileNames count] -1;
+		var selectedPaths = [];
 
-		var imagesCollection = [],
-			fileManager = [NSFileManager defaultManager];
 
-		if(tools.majorVersion() == 3){
-			var scriptPath = sketch.scriptPath;
-			var pluginFolder = scriptPath.match(/Plugins\/([\w -])*/)[0] + '/';
-			var sketchPluginsPath = scriptPath.replace(/Plugins([\w \/ -])*.sketchplugin$/, '');
-			imagesPath =  sketchPluginsPath + pluginFolder + dataPath;
-		}
-		log('imagesPath: '+imagesPath);
-		var imagesFileNames = [fileManager contentsOfDirectoryAtPath:imagesPath error:nil],
-			imgLen = [imagesFileNames count];
+		if(imgAmount > imageCount){
+			while(imgAmount--) {
+				var index = Math.floor(Math.random() * imageCount);
+				do {
+					index = index > imageCount ? 0 : index + 1;
+					var fileName = imagesFileNames[index];
+					var filePath = imagesPath + fileName;			
+				} while(![fileManager fileExistsAtPath: filePath] || fileName == '.DS_Store')
 
-		log('imagesFileNames (before clean): '+imagesFileNames);
-
-		// clean out invalid files
-		var imagesFileNamesCleaned = [];
-		for (var i = 0; i < imgLen; i++) {
-			if (imagesFileNames[i] == '.DS_Store') {
-				log('skipping '+imagesFileNames[i]);
-
-			} else {
-				imagesFileNamesCleaned.push(imagesFileNames[i]);
+				selectedPaths.push(filePath);
+			}
+		} 
+		else{
+			while(imgAmount--) {
+				do {
+					var index = Math.floor(Math.random() * imageCount);
+					var fileName = imagesFileNames[index];
+					var filePath = imagesPath + fileName;
+					var match = selectedPaths.filter(function(selectedPath){return filePath == selectedPath;});
+				} while(fileName == '.DS_Store' || ![fileManager fileExistsAtPath: filePath] || match.length >= 1);
+				selectedPaths.push(filePath);
 			}
 		}
-
-		log('imagesFileNamesCleaned: '+imagesFileNamesCleaned);
-
-		var offset = 0;
-		for (var i = 0; i < selectedImgCount; i++) {
-      var filename = '';
-			if (i+1 > imagesFileNamesCleaned.length) {
-				// no more images (reached end) - move back and use the first ones
-				filename = imagesFileNamesCleaned[offset++];
-			} else {
-				filename = imagesFileNamesCleaned[i];
+		
+		return selectedPaths.map(function(imagePath){
+			if ([fileManager fileExistsAtPath: imagePath]) {				
+				var image = [[NSImage alloc] initWithContentsOfFile:imagePath];			
+				return image;
 			}
+		})
+	}
 
-			var pathToFile = imagesPath+filename;
-
-			if ([fileManager fileExistsAtPath: pathToFile]) {
-				var newImage = [[NSImage alloc] initWithContentsOfFile:pathToFile];
-				imagesCollection.push(newImage);
-			}
-		}
-
-		return imagesCollection;
-	};
-
-	function main(){
+	function main(){		
 		var allLayers = [[doc currentPage] layers],
-			imagesCollection = loadImages([selection count]);
+			imagesCollection = loadImages([selection count] + 1);
 
 		for(var i = 0; i < [selection count]; i++){
 			var layer = selection[i];
             if([layer class] == MSShapeGroup){
                 var image = imagesCollection[i];
                 var fill = layer.style().fills().firstObject();
-                fill.setFillType(4);
-                //log(tools.minorVersion())
+                fill.setFillType(4);                
                 if(tools.minorVersion() >= 1){
-                	var coll = layer.style().fills().firstObject().documentData().images();
+                	var coll = layer.style().fills().firstObject().documentData().images();              
                 	[fill setPatternImage:image collection:coll]
                 }
                 else{
                 	layer.style().fills().firstObject().setPatternImage( image );
-                }
+                }                                                
                 layer.style().fills().firstObject().setPatternFillType(1);
             }
 		}
 
-		if([selection count] == 0) alert('Select at least one vector shape');
+		if([selection count] == 0) [doc showMessage:'Select at least one vector shape'];;
 	}
 	main();
-};
+}
+
